5d16e8b4e883c19f3e29b5a44aa80f039745b534
Andy Schulz <andy.schulz@test-steps.de>
2023-11-08 15:48:29 +0100

    feat: add Websocket ability

---
+++ thekla4j-websocket/src/main/java/com/teststeps/thekla4j/websocket/stomp/spp/abilities/UseWebsocketWithStomp.java
@@ -0,0 +1,10 @@
+package com.teststeps.thekla4j.websocket.stomp.spp.abilities;
+
+import com.teststeps.thekla4j.core.base.abilities.Ability;
+
+public class UseWebsocketWithStomp implements Ability {
+  @Override
+  public void destroy() {
+
+  }
+}
