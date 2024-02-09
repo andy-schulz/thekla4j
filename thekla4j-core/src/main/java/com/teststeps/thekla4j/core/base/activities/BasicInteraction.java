5c9125512687c7c398c4ce9a8f9d397504f03bce
Andy Schulz <andy.schulz@test-steps.de>
2024-02-09 16:32:48 +0100

    [core] feat: add BasicInteraction activity with <Void, Void>

---
+++ thekla4j-core/src/main/java/com/teststeps/thekla4j/core/base/activities/BasicInteraction.java
@@ -0,0 +1,28 @@
+package com.teststeps.thekla4j.core.base.activities;
+
+import com.teststeps.thekla4j.commons.error.ActivityError;
+import com.teststeps.thekla4j.core.base.errors.TaskIsNotEvaluated;
+import com.teststeps.thekla4j.core.base.persona.Actor;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import lombok.NonNull;
+
+public abstract class BasicInteraction implements Activity<Void, Void> {
+
+  private Option<Either<ActivityError, Void>> evaluationResult = Option.none();
+
+
+  @Override
+  final public Either<ActivityError, Void> perform(@NonNull Actor actor, Void unused){
+    Either<ActivityError, Void> res = performAs(actor);
+    evaluationResult = Option.of(res);
+    return res;
+  };
+
+  @Override
+  final public Either<ActivityError, Void> value() throws TaskIsNotEvaluated {
+    return evaluationResult.getOrElseThrow(() -> TaskIsNotEvaluated.called(this));
+  }
+
+  protected abstract Either<ActivityError, Void> performAs(Actor actor);
+}
