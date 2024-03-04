743b05f0bbfe796af7465dce43450c820c4be541
Andy Schulz <andy.schulz@test-steps.de>
2024-03-04 06:07:55 +0100

    [browser] feat: add name to Element

--- thekla4j-browser/core/src/main/java/com/teststeps/thekla4j/browser/core/Element.java
+++ thekla4j-browser/core/src/main/java/com/teststeps/thekla4j/browser/core/Element.java
@@ -12,20 +12,22 @@ import static com.teststeps.thekla4j.browser.core.status.ElementStatusFunctions.
 @With
 public record Element(
     List<Locator> locators,
+    String name,
     UntilElement waiter) {
   public static Element found(Locator locator) {
     return new Element(
         List.of(locator),
+        "unnamed",
         defaultWaiter());
   }
 
   public String toString() {
-    return String.format("Element.found(By(%s))", this.locators
+    return String.format("Element<%s> found By (%s)", name, this.locators
         .map(Object::toString)
         .collect(Collectors.joining(" > ")));
   }
 
   public Element wait(UntilElement waiter) {
-    return new Element(locators, waiter);
+    return new Element(locators, name, waiter);
   }
 }
