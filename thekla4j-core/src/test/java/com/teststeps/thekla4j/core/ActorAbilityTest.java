4e97cfde0cf670ba2793d5355aa6809d39d510ae
Andy Schulz <andy.schulz@test-steps.de>
2024-06-25 13:21:31 +0200

    [core] tests: add actor tests

--- thekla4j-core/src/test/java/com/teststeps/thekla4j/core/ActorAbilityTest.java
+++ thekla4j-core/src/test/java/com/teststeps/thekla4j/core/ActorAbilityTest.java
@@ -1,14 +1,87 @@
 package com.teststeps.thekla4j.core;
 
 import com.teststeps.thekla4j.core.base.abilities.Ability;
+import com.teststeps.thekla4j.core.base.errors.DoesNotHaveTheAbility;
 import com.teststeps.thekla4j.core.base.persona.Actor;
 import com.teststeps.thekla4j.core.base.persona.Cast;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class ActorAbilityTest {
+
+
+  @Test
+  public void actorHasName() {
+    Actor actor = Actor.named("TheActor");
+
+    assertThat("check actor has name", actor.getName(), equalTo("TheActor"));
+  }
+
+  @Test
+  public void actorHasNoAbilities() {
+    Actor actor = Actor.named("actor");
+
+    assertThat("check actor has no ability", actor.can(FirstAbility.class), equalTo(false));
+  }
+
+  @Test
+  public void actorHasOneAbility() {
+    Actor actor = Actor.named("actor");
+
+    FirstAbility ability = mock(FirstAbility.class);
+
+    actor.whoCan(ability);
+
+    assertThat("check actor has ability", actor.can(FirstAbility.class), equalTo(true));
+  }
+
+  @Test
+  public void actorHasMultipleAbilities() {
+    Actor actor = Actor.named("actor");
+
+    FirstAbility firstAbility = new FirstAbility();
+    SecondAbility secondAbility = new SecondAbility();
+
+    actor.whoCan(firstAbility);
+    actor.whoCan(secondAbility);
+
+    assertThat("check actor has first ability", actor.can(FirstAbility.class), equalTo(true));
+    assertThat("check actor has second ability", actor.can(SecondAbility.class), equalTo(true));
+  }
+
+  @Test
+  public void actorRetrievesAbility() throws DoesNotHaveTheAbility {
+    Actor actor = Actor.named("actor");
+
+    FirstAbility firstAbility = new FirstAbility();
+    SecondAbility secondAbility = new SecondAbility();
+
+    actor.whoCan(firstAbility);
+    actor.whoCan(secondAbility);
+
+    Ability retrievedFirstAbility = actor.withAbilityTo(FirstAbility.class);
+    Ability retrievedSecondAbility = actor.withAbilityTo(SecondAbility.class);
+
+    assertThat("check actor has first ability", retrievedFirstAbility, equalTo(firstAbility));
+    assertThat("check actor has second ability", retrievedSecondAbility, equalTo(secondAbility));
+  }
+
+  @Test void actorHasMultipleAbilitiesAddedInOneCall() {
+    Actor actor = Actor.named("actor");
+
+    FirstAbility firstAbility = new FirstAbility();
+    SecondAbility secondAbility = new SecondAbility();
+
+    actor.whoCan(firstAbility, secondAbility);
+
+    assertThat("check actor has first ability", actor.can(FirstAbility.class), equalTo(true));
+    assertThat("check actor has second ability", actor.can(SecondAbility.class), equalTo(true));
+  }
+
 
   @Test
   public void destroyAbilitiesOfCast() {
@@ -16,7 +89,7 @@ public class ActorAbilityTest {
 
     Actor actor = cast.callActorToStageNamed("actor");
 
-    DestroyFistAbilities testDestroyAbilities = mock(DestroyFistAbilities.class);
+    FirstAbility testDestroyAbilities = mock(FirstAbility.class);
 
     actor.whoCan(testDestroyAbilities);
 
@@ -30,8 +103,8 @@ public class ActorAbilityTest {
 
     Actor actor = Actor.named("actor");
 
-    DestroyFistAbilities testDestroyAbilities = mock(DestroyFistAbilities.class);
-    DestroySecondAbilities testDestroySecondAbilities = mock(DestroySecondAbilities.class);
+    FirstAbility testDestroyAbilities = mock(FirstAbility.class);
+    SecondAbility testDestroySecondAbilities = mock(SecondAbility.class);
 
     actor.whoCan(testDestroyAbilities);
     actor.whoCan(testDestroySecondAbilities);
@@ -44,7 +117,7 @@ public class ActorAbilityTest {
 
 
 
-  static class DestroyFistAbilities implements Ability {
+  static class FirstAbility implements Ability {
 
     @Override
     public void destroy() {
@@ -52,7 +125,7 @@ public class ActorAbilityTest {
     }
   }
 
-  static class DestroySecondAbilities implements Ability {
+  static class SecondAbility implements Ability {
 
     @Override
     public void destroy() {
