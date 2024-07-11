d1ab169c9f95922c2c293ebfbf65d953730f9506
Andy Schulz <andy.schulz@test-steps.de>
2024-07-11 16:25:48 +0200

    [core] docs: add javadoc to API utility class

--- thekla4j-core/src/main/java/com/teststeps/thekla4j/core/activities/API.java
+++ thekla4j-core/src/main/java/com/teststeps/thekla4j/core/activities/API.java
@@ -5,24 +5,62 @@ import io.vavr.control.Try;
 
 public class API {
 
+  /**
+   * create a map task that applies the given mapper function to the input
+   *
+   * @param mapper the mapper function
+   * @return the map task
+   * @param <T> the input type
+   * @param <R> the output type
+   */
   public static <T, R> Map<T, R> map(Function1<T, R> mapper) {
     return new Map<>(r -> Try.of(() -> mapper.apply(r)));
   }
 
-  //  public static <T> Map<T, Void> map(Consumer<T> consumer) {
-//    return new Map<>(r -> Try.run(() -> consumer.accept(r)));
-//
-//  }
+  /**
+   * create a map task that applies the given mapper function to the input and passes the given reason to the task
+   * the reason is used in the activity log to describe the task
+   *
+   * @param mapper  the mapper function
+   * @param reason the description what the task is intended to do
+   * @return the map task
+   * @param <T> the input type
+   * @param <R> the output type
+   */
   public static <T, R> Map<T, R> map(Function1<T, R> mapper, String reason) {
     return new Map<>(r -> Try.of(() -> mapper.apply(r)), reason);
   }
 
+  /**
+   * create a map task that applies the given mapper function to the input, the mapper function returns a Try which is converted to an Either
+   * when the task is executed and fails, the error is logged in the activity log
+   *
+   * @param mapper the mapper function
+   * @return the map task
+   * @param <T> the input type
+   * @param <R> the output type
+   */
   public static <T, R> Map<T, R> mapTry(Function1<T, Try<R>> mapper) {
     return new Map<>(mapper);
   }
 
+  /**
+   * create a map task that applies the given mapper function to the input, the mapper function returns a Try which is converted to an Either
+   * when the task is executed and fails, the error is logged in the activity log
+   * the reason is used in the activity log to describe the task
+   *
+   * @param mapper the mapper function
+   * @param reason the description what the task is intended to do
+   * @return the map task
+   * @param <T> the input type
+   * @param <R> the output type
+   */
   public static <T, R> Map<T, R> mapTry(Function1<T, Try<R>> mapper, String reason) {
     return new Map<>(mapper, reason);
+  }
+
+  private API() {
+    // prevent instantiation
   }
 
 }
