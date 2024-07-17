06d3dd43eb35afc7990cd774a34b7013aab7b374
Andy Schulz <andy.schulz@test-steps.de>
2024-07-17 18:21:52 +0200

    [browser] feat: add additional keys

--- thekla4j-browser/core/src/main/java/com/teststeps/thekla4j/browser/spp/activities/keyActions/Key.java
+++ thekla4j-browser/core/src/main/java/com/teststeps/thekla4j/browser/spp/activities/keyActions/Key.java
@@ -2,5 +2,17 @@ package com.teststeps.thekla4j.browser.spp.activities.keyActions;
 
 public enum Key {
   TAB,
-  ENTER;
+  ENTER,
+  ESCAPE,
+  SPACE,
+  ARROW_DOWN,
+  ARROW_UP,
+  ARROW_LEFT,
+  ARROW_RIGHT,
+  BACK_SPACE,
+  DELETE,
+  CONTROL,
+  SHIFT,
+  ALT,
+  COMMAND;
 }
