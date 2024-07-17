ebb46430b36c108fa05b79dbdad7d07c2e9db505
Andy Schulz <andy.schulz@test-steps.de>
2024-07-17 19:44:32 +0200

    [browser] feat: implement setting browserstack status via js executor

---
+++ thekla4j-browser/selenium/src/test/java/com/teststeps/thekla4j/browser/selenium/integration/IT_BrowserStackStatus.java
@@ -0,0 +1,36 @@
+package com.teststeps.thekla4j.browser.selenium.integration;
+
+import com.teststeps.thekla4j.browser.selenium.Selenium;
+import com.teststeps.thekla4j.browser.selenium.spp.activities.SetBrowserstackStatus;
+import com.teststeps.thekla4j.browser.spp.abilities.BrowseTheWeb;
+import com.teststeps.thekla4j.browser.spp.activities.Navigate;
+import com.teststeps.thekla4j.commons.error.ActivityError;
+import com.teststeps.thekla4j.core.base.persona.Actor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.function.Function;
+
+public class IT_BrowserStackStatus {
+
+  Actor actor;
+
+  @BeforeEach
+  public void setUp() {
+    actor = Actor.named("Test Actor")
+      .whoCan(BrowseTheWeb.with(Selenium.browser()));
+  }
+
+  @AfterEach
+  public void tearDown() {
+    actor.cleansStage();
+  }
+
+//  @Test
+  public void testBrowserStackStatus() throws ActivityError {
+    actor.attemptsTo(
+      Navigate.to("https://www.browserstack.com/status"),
+      SetBrowserstackStatus.ofTestCaseToFailed("The name of the test case", "Test failed"))
+      .getOrElseThrow(Function.identity());
+  }
+}
