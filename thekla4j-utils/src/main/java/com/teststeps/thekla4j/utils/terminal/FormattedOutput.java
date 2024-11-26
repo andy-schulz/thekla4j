b7153021cf325b1dffd22d2915f286e6f674820d
Andy Schulz <andy.schulz@test-steps.de>
2024-11-26 14:35:43 +0100

    [utils] feat: add formatted output utility class

---
+++ thekla4j-utils/src/main/java/com/teststeps/thekla4j/utils/terminal/FormattedOutput.java
@@ -0,0 +1,230 @@
+package com.teststeps.thekla4j.utils.terminal;
+
+import io.vavr.Function2;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FormattedOutput {
+
+  private static final String ANSI_RESET = "\u001B[0m";
+  private static final String ANSI_BLACK = "\u001B[30m";
+  private static final String ANSI_RED = "\u001B[31m";
+  private static final String ANSI_GREEN = "\u001B[32m";
+  private static final String ANSI_YELLOW = "\u001B[33m";
+  private static final String ANSI_BLUE = "\u001B[34m";
+  private static final String ANSI_PURPLE = "\u001B[35m";
+  private static final String ANSI_CYAN = "\u001B[36m";
+  private static final String ANSI_WHITE = "\u001B[37m";
+
+  private static final String BOLD = "\u001B[1m";
+  private static final String ITALIC = "\u001B[3m";
+  private static final String UNDERLINE = "\u001B[4m";
+
+  private static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+  private static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+  private static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+  private static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+  private static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+  private static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+  private static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+  private static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+
+  private static final Function2<String, String, String> addColorMultiline =
+    (text, color) -> Stream.of(text.split("\n")).map(t -> color + t + ANSI_RESET)
+      .collect(Collectors.joining("\n"));
+
+  /**
+   * Add color BLACK to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String BLACK(String text) {
+    return addColorMultiline.apply(text, ANSI_BLACK);
+  }
+
+  /**
+   * Add color RED to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String RED(String text) {
+    return addColorMultiline.apply(text, ANSI_RED);
+  }
+
+  /**
+   * Add color GREEN to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String GREEN(String text) {
+    return addColorMultiline.apply(text, ANSI_GREEN);
+  }
+
+  /**
+   * Add color YELLOW to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String YELLOW(String text) {
+    return addColorMultiline.apply(text, ANSI_YELLOW);
+  }
+
+  /**
+   * Add color BLUE to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String BLUE(String text) {
+    return addColorMultiline.apply(text, ANSI_BLUE);
+  }
+
+  /**
+   * Add color PURPLE to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String PURPLE(String text) {
+    return addColorMultiline.apply(text, ANSI_PURPLE);
+  }
+
+  /**
+   * Add color CYAN to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String CYAN(String text) {
+    return addColorMultiline.apply(text, ANSI_CYAN);
+  }
+
+  /**
+   * Add color WHITE to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String WHITE(String text) {
+    return addColorMultiline.apply(text, ANSI_WHITE);
+  }
+
+  /**
+   * make the text BOLD
+   *
+   * @param text  text to format
+   * @return formatted text
+   */
+  public static String BOLD(String text) {
+    return addColorMultiline.apply(text, BOLD);
+  }
+
+  /**
+   * make the text ITALIC
+   *
+   * @param text  text to format
+   * @return formatted text
+   */
+  public static String ITALIC(String text) {
+    return addColorMultiline.apply(text, ITALIC);
+  }
+
+  /**
+   * make the text UNDERLINE
+   *
+   * @param text  text to format
+   * @return formatted text
+   */
+  public static String UNDERLINE(String text) {
+    return addColorMultiline.apply(text, UNDERLINE);
+  }
+
+  /**
+   * Add color BLACK_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String BLACK_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_BLACK_BACKGROUND);
+  }
+
+  /**
+   * Add color RED_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String RED_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_RED_BACKGROUND);
+  }
+
+  /**
+   * Add color GREEN_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String GREEN_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_GREEN_BACKGROUND);
+  }
+
+  /**
+   * Add color YELLOW_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String YELLOW_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_YELLOW_BACKGROUND);
+  }
+
+  /**
+   * Add color BLUE_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String BLUE_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_BLUE_BACKGROUND);
+  }
+
+  /**
+   * Add color PURPLE_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String PURPLE_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_PURPLE_BACKGROUND);
+  }
+
+  /**
+   * Add color CYAN_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String CYAN_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_CYAN_BACKGROUND);
+  }
+
+  /**
+   * Add color WHITE_BACKGROUND to the text
+   *
+   * @param text  text to color
+   * @return colored text
+   */
+  public static String WHITE_BG(String text) {
+    return addColorMultiline.apply(text, ANSI_WHITE_BACKGROUND);
+  }
+
+  private FormattedOutput() {
+  }
+}
---
+++ thekla4j-utils/src/test/java/FormattedOutputTest.java
@@ -0,0 +1,101 @@
+import com.teststeps.thekla4j.utils.terminal.FormattedOutput;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FormattedOutputTest {
+
+  // write testes to check the ColoredOutput utility class
+
+  @Test
+  public void redFont() {
+    String font = FormattedOutput.RED("Hello World");
+    assertThat("font is red", font, equalTo("\u001B[31mHello World\u001B[0m"));
+
+    String background = FormattedOutput.RED_BG("Hello World");
+    assertThat("background is red", background, equalTo("\u001B[41mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void greenFont() {
+    String font = FormattedOutput.GREEN("Hello World");
+    assertThat("font is green", font, equalTo("\u001B[32mHello World\u001B[0m"));
+
+    String background = FormattedOutput.GREEN_BG("Hello World");
+    assertThat("background is green", background, equalTo("\u001B[42mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void yellowFont() {
+    String font = FormattedOutput.YELLOW("Hello World");
+    assertThat("font is yellow", font, equalTo("\u001B[33mHello World\u001B[0m"));
+
+    String background = FormattedOutput.YELLOW_BG("Hello World");
+    assertThat("background is yellow", background, equalTo("\u001B[43mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void blueFont() {
+    String font = FormattedOutput.BLUE("Hello World");
+    assertThat("font is blue", font, equalTo("\u001B[34mHello World\u001B[0m"));
+
+    String background = FormattedOutput.BLUE_BG("Hello World");
+    assertThat("background is blue", background, equalTo("\u001B[44mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void purpleFont() {
+    String font = FormattedOutput.PURPLE("Hello World");
+    assertThat("font is purple", font, equalTo("\u001B[35mHello World\u001B[0m"));
+
+    String background = FormattedOutput.PURPLE_BG("Hello World");
+    assertThat("background is purple", background, equalTo("\u001B[45mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void cyanFont() {
+    String font = FormattedOutput.CYAN("Hello World");
+    assertThat("font is cyan", font, equalTo("\u001B[36mHello World\u001B[0m"));
+
+    String background = FormattedOutput.CYAN_BG("Hello World");
+    assertThat("background is cyan", background, equalTo("\u001B[46mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void whiteFont() {
+    String font = FormattedOutput.WHITE("Hello World");
+    assertThat("font is white", font, equalTo("\u001B[37mHello World\u001B[0m"));
+
+    String background = FormattedOutput.WHITE_BG("Hello World");
+    assertThat("background is white", background, equalTo("\u001B[47mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void blackFont() {
+    String font = FormattedOutput.BLACK("Hello World");
+    assertThat("font is black", font, equalTo("\u001B[30mHello World\u001B[0m"));
+
+    String background = FormattedOutput.BLACK_BG("Hello World");
+    assertThat("background is black", background, equalTo("\u001B[40mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void boldFont() {
+    String font = FormattedOutput.BOLD("Hello World");
+    assertThat("font is bold", font, equalTo("\u001B[1mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void underlineFont() {
+    String font = FormattedOutput.UNDERLINE("Hello World");
+    assertThat("font is underline", font, equalTo("\u001B[4mHello World\u001B[0m"));
+  }
+
+  @Test
+  public void italicFont() {
+    String font = FormattedOutput.ITALIC("Hello World");
+    assertThat("font is italic", font, equalTo("\u001B[3mHello World\u001B[0m"));
+  }
+
+}
