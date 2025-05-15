2c92c5d2e8d30804fa08abce79ef8dbec0be86f6
Andy Schulz <andy.schulz@test-steps.de>
2025-05-15 11:41:21 +0200

    [selenium] doc: fix javadoc parameter name

--- thekla4j-browser/selenium/src/main/java/com/teststeps/thekla4j/browser/selenium/config/SeleniumConfig.java
+++ thekla4j-browser/selenium/src/main/java/com/teststeps/thekla4j/browser/selenium/config/SeleniumConfig.java
@@ -11,35 +11,35 @@ public record SeleniumConfig(
 
   /**
    * the remote url for the selenium grid server
-   * @param browserName - the remote url for the selenium grid server
+   * @param remoteUrl - the remote url for the selenium grid server
    * @return - the remote url for the selenium grid server
    */
   String remoteUrl,
 
   /**
    * activate the local file detector
-   * @param browserName - the local file detector
+   * @param setLocalFileDetector - the local file detector
    * @return - the local file detector
    */
   Boolean setLocalFileDetector,
 
   /**
    * the browserstack options
-   * @param browserName - the browserstack options
+   * @param bStack - the browserstack options
    * @return - the browserstack options
    */
   BrowsersStackOptions bStack,
 
   /**
    * the selenium options
-   * @param browserName - the selenium options
+   * @param seOptions - the selenium options
    * @return - the selenium options
    */
   SeleniumOptions seOptions,
 
   /**
    * the capabilities
-   * @param browserName - the capabilities
+   * @param capabilities - the capabilities
    * @return - the capabilities
    */
   Map<String, Map<String, String>> capabilities
